e44701c030ab854e8142661b46149e8226b7d783
perhac <abdullah13112004@gmail.com>
2024-10-01 16:37:06 +0530

    feat!: integrated factory design pattern

---
+++ include/school_factory.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <memory>
+#include "school.h"
+#include "class.h"
+#include "student.h"
+
+class SchoolFactory {
+    public:
+    static std::unique_ptr<School> createSchool();
+    static std::unique_ptr<Class> createClass();
+    static std::unique_ptr<Student> createStudent();
+};
